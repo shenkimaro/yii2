a1897ef2c6c5f197022d7bc7f69a6c6f876ff2de
shenkimaro <ibanez.almeida@hotmail.com>
2024-07-07 11:36:47 -0300

    Configuração para conexao ao bd

--- backend/app/basic/config/db.php
+++ backend/app/basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db;dbname=processo_seletivo',
     'username' => 'root',
-    'password' => '',
+    'password' => 'teste',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
